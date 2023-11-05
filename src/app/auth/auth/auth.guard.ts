d2733fa2746dc84a12d93f64da2b11164eb3e094
JiwonKim <jaceyjiwon.kim@gmail.com>
2023-11-05 01:22:51 -0400

    ngrx added

--- src/app/auth/auth/auth.guard.ts
+++ src/app/auth/auth/auth.guard.ts
@@ -1,38 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-// import { AuthService } from '../../service/auth.service';
-// import { Observable, map } from 'rxjs';
-// import { SnackbarService } from '../../service/snackbar.service';
-
-// @Injectable()
-
-// export class AuthGuard implements CanActivate {
-//   constructor(
-//     private authService: AuthService, 
-//     private snackbarService: SnackbarService,
-//     private router: Router) {}
-
-//   canActivate(
-//     route: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot
-//   ): Observable<boolean | UrlTree> {
-//     return this.authService._isLoggedIn.pipe(
-//       map((isAuth) => {
-//         if (isAuth) {
-//           return true; // User is authenticated, allow access
-//         } else {
-//           // User is not authenticated, navigate to login page
-//           const loginUrl = this.router.createUrlTree(['/login']);
-//           this.snackbarService.show('You must log in first!', 'warning')
-//           return loginUrl;
-//         }
-//       })
-//     );
-//   }
-
-// }
-
-// Below is the code for the auth.guard.ts file with the NGRX implementation:
 
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
